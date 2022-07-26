df11c06128e501ff36b4b0c4e84623355459ac3c
rochelle-mellomida <110008119+rochelle-mellomida@users.noreply.github.com>
2022-07-26 16:19:57 -0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let video = document.getElementById('video');
-
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
-    navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
-        video.srcObject = stream;
-        video.play();
-    })
-}
